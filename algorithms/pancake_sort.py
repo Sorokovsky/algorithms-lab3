75f92702dae4d3aa44c9d5c8ea5df64a61813c06
Sorokovsky <Sorokovskys@ukr.net>
2025-09-20 15:10:15 +0300

    Pancake sort

--- algorithms/pancake_sort.py
+++ algorithms/pancake_sort.py
@@ -10,18 +10,24 @@ class PancakeSort(SortingAlgorithm):
     def _sort(self, array: list[int], is_order_correct: Callable[[int, int], bool]) -> list[int]:
         result = array.copy()
         count = len(result)
-        for index in range(count):
-            target_index = self._find_index_by_rule(result, is_order_correct)
+        if count <= 1:
+            return result
+        for i in range(count - 1, -1, -1):
+            target_index = self._find_index_by_rule(result, is_order_correct, i + 1)
             self._increment_iteration()
-            if target_index != index - 1:
-                result[:target_index + 1] = reversed(result[:target_index + 1])
-                result[:index] = reversed(result[:index])
+            if target_index != i:
+                if target_index != 0:
+                    result[:target_index + 1] = reversed(result[:target_index + 1])
+                    self._increment_iteration()
+                if i > 0:
+                    result[:i + 1] = reversed(result[:i + 1])
+                    self._increment_iteration()
         return result
 
-    def _find_index_by_rule(self, array: list[int], is_order_correct: Callable[[int, int], bool]) -> int:
+    def _find_index_by_rule(self, array: list[int], is_order_correct: Callable[[int, int], bool], size: int) -> int:
         target_index = 0
-        for index in range(len(array)):
+        for index in range(size):
             self._increment_iteration()
-            if array[index] > array[target_index]:
+            if not is_order_correct(array[index], array[target_index]):
                 target_index = index
         return target_index
